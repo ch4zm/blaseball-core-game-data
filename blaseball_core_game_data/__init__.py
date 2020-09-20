99510f96da185ae6eef9def6291566ae14c43f70
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-09-20 16:53:31 -0700

    auto-update to version 0.7.2

--- blaseball_core_game_data/__init__.py
+++ blaseball_core_game_data/__init__.py
@@ -3,7 +3,7 @@ import os
 import json
 
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 data_path = os.path.abspath(os.path.join(root_path, 'data'))
